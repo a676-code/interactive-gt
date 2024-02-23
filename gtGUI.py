00353b06cc350a2887925161961333031cfa1989
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-02-22 20:20:31 -0500

    improved save payoffs button

--- gtGUI.py
+++ gtGUI.py
@@ -11,6 +11,7 @@ def numStratsClick():
     L = payoffMatrixFrame.grid_slaves()
     for l in L:
         l.grid_remove()
+        print(l.get())
     
     # refilling the table
     numStrats1 = int(numStratsEntry1.get())
@@ -21,18 +22,37 @@ def numStratsClick():
         for j in range(numStrats2):
             e = Entry(payoffMatrixFrame, width=5)
             e.grid(row=i, column=j, sticky=NSEW)
-            e.insert(END, '%d,%d' % (i, j))
+            e.insert(END, '%d, %d' % (0, 0))
             cols.append(e)
         rows.append(cols)
     
     root.geometry(f"{45 * numStrats2 + 400}x{25 * numStrats1 + 150}")
     
 def savePayoffs():
+    L = payoffMatrixFrame.grid_slaves()
+    payoffs = [tuple(map(int, l.get().split(", "))) for l in L]
+    payoffs.reverse()
+    print(payoffs)
+    numStrats1 = int(numStratsEntry1.get())
+    numStrats2 = int(numStratsEntry2.get())
+    
+    # converting the list of payoffs to a list of lists
+    newPayoffs = []
+    row = []
+    numInRow = 0
+    for p in payoffs:
+        if numInRow < 2:
+            row.append(p)
+            numInRow += 1
+            if numInRow == 2:
+                newPayoffs.append(row)
+                numInRow = 0
+                row = []        
     return
 
 root = Tk()
 root.title("Interactive GT")
-root.geometry("550x250")
+root.geometry("550x500")
 root.iconbitmap("knight.ico")
 
 # numStrats Frame
@@ -45,6 +65,7 @@ numStratsEntry2 = Entry(numStratsFrame, width=5)
 numStratsEntry1.insert(0, "2")
 numStratsEntry2.insert(0, "2")
 numStratsButton = Button(numStratsFrame, text="Enter", command=numStratsClick)
+warning = Label(numStratsFrame, text="Warning: pressing Enter will clear all payoffs")
 
 # Payoffs Frame
 payoffsFrame = LabelFrame(root, text="Payoffs", padx=10, pady=10)
@@ -57,7 +78,7 @@ for i in range(int(numStratsEntry1.get())):
     for j in range(int(numStratsEntry2.get())):
         e = Entry(payoffMatrixFrame, width=5)
         e.grid(row=i, column=j, sticky=NSEW)
-        e.insert(END, '%d,%d' % (i, j))
+        e.insert(END, '%d, %d' % (0, 0))
         cols.append(e)
     rows.append(cols)
     
@@ -68,6 +89,8 @@ equilibriaFrame = LabelFrame(root, text="Equilibria", padx=10, pady=10)
 
 equilibriaButton = Button(equilibriaFrame, text="Compute Equilibria", command=computeEquilibria)
 
+output = Label(equilibriaFrame, text="EQUILIBRIA HERE", bd=1, relief=SUNKEN, anchor=E)
+
 # Putting everything on the screen
 numStratsFrame.grid(row=0, column=0, padx=10, pady=10)
 numStratsLabel1.grid(row=0, column=0)
@@ -75,13 +98,14 @@ numStratsLabel2.grid(row=1, column=0)
 numStratsEntry1.grid(row=0, column=1)
 numStratsEntry2.grid(row=1, column=1)
 numStratsButton.grid(row=0, column=2, padx=5, pady=5)
+warning.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
 payoffsFrame.grid(row=0, column=1, padx=10, pady=10)
-
 payoffMatrixFrame.grid(row=0, column=0, padx=10, pady=10)
 savePayoffsButton.grid(row=1, column=0, padx=5, pady=5)
 
-equilibriaFrame.grid(row=0, column=2, padx=10, pady=10)
+equilibriaFrame.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 equilibriaButton.pack(padx=10, pady=10)
+output.pack(padx=5, pady=5)
 
 root.mainloop()
