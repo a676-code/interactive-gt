361456e2f34e114a940bc09bfde55f024faa6727
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2024-03-17 18:40:20 -0400

    added inert widgets to equilibriaPanel and axelrodPanel

--- wxinteractivegt.py
+++ wxinteractivegt.py
@@ -70,20 +70,38 @@ class EquilibriaPanel(wx.Panel):
     def __init__(self, parent):
         super(EquilibriaPanel, self).__init__(parent)
         
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(wx.StaticText(self, label="Equilibria"))
+        # Creating widgets
+        labelList = ["Standard nashpy Output", "Named Strategies"]
+        radiobox = wx.RadioBox(self, choices = labelList, style=wx.RA_SPECIFY_ROWS)
+        equilibriaButton = wx.Button(self, label="Compute Equilibria")
+        output = wx.TextCtrl(self, size=(200, 100), style=wx.TE_MULTILINE)
+        
+        # Putting the widgets in the sizer
+        equilibriaSizer = wx.GridBagSizer(3, 2)
+        equilibriaSizer.Add(wx.StaticText(self, label="Equilibria"), pos=(0, 0))
+        equilibriaSizer.Add(radiobox, pos=(1, 0))
+        equilibriaSizer.Add(equilibriaButton, pos=(1, 1))
+        equilibriaSizer.Add(output, pos=(2, 0))
+        
+        equilibriaSizer.AddGrowableRow(0)
+        equilibriaSizer.AddGrowableRow(1)
+        equilibriaSizer.AddGrowableRow(2)
+        equilibriaSizer.AddGrowableCol(0)
+        equilibriaSizer.AddGrowableCol(1)
+        
+        self.SetSizer(equilibriaSizer)
         
 class IESDSPanel(wx.Panel):
     def __init__(self, parent):
         super(IESDSPanel, self).__init__(parent)
         
-        # creating widgets
+        # Creating widgets
         labelList = ["Full Computation", "Computation in Steps"]
         radiobox = wx.RadioBox(self, choices = labelList, style=wx.RA_SPECIFY_ROWS)
         revertButton = wx.Button(self, label="Revert")
         iesdsButton = wx.Button(self, label="Eliminate Strictly Dominated Strategies")
         
-        # putting the widgets in the sizer
+        # Putting the widgets in the sizer
         iesdsSizer = wx.GridBagSizer(3, 2)
         iesdsSizer.Add(wx.StaticText(self, label="IESDS"), pos=(0, 0), flag=wx.ALIGN_LEFT)
         iesdsSizer.Add(radiobox, pos=(1, 0))
@@ -103,8 +121,50 @@ class AxelrodPanel(wx.Panel):
     def __init__(self, parent):
         super(AxelrodPanel, self).__init__(parent)
         
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(wx.StaticText(self, label="axelrod"))
+        strategies = ["Alternator", "Tit For Tat"]
+        
+        # Creating widgets
+        stratText1 = wx.StaticText(self, label="Enter a strategy for player 1; ")
+        strat1Combobox = wx.ComboBox(self, choices=strategies)
+        stratText2 = wx.StaticText(self, label="Enter a strategy for player 2: ")
+        strat2Combobox = wx.ComboBox(self, choices=strategies)
+        numTurnsText = wx.StaticText(self, label="Enter the number of turns: ")
+        numTurnsTextCtrl = wx.TextCtrl(self)
+        labelList = ["Add to Database", "Don't Add to Database"]
+        radioButton1 = wx.RadioButton(self, label="Add to Database", style=wx.RB_GROUP)
+        playMatchButton = wx.Button(self, label="Play Match")
+        radioButton2 = wx.RadioButton(self, label="Don't Add to Database")
+        viewDatabaseButton = wx.Button(self, label="View Database")
+        matchTextCtrl = wx.TextCtrl(self)
+        scoreTextCtrl = wx.TextCtrl(self)
+        
+        # Putting the widgets in the sizer
+        axelrodSizer = wx.GridBagSizer(7, 2)
+        axelrodSizer.Add(wx.StaticText(self, label="axelrod"), pos=(0, 0))
+        axelrodSizer.Add(stratText1, pos=(1, 0))
+        axelrodSizer.Add(strat1Combobox, pos=(1, 1))
+        axelrodSizer.Add(stratText2, pos=(2, 0))
+        axelrodSizer.Add(strat2Combobox, pos=(2, 1))
+        axelrodSizer.Add(numTurnsText, pos=(3, 0))
+        axelrodSizer.Add(numTurnsTextCtrl, pos=(3, 1))
+        axelrodSizer.Add(radioButton1, pos=(4, 0))
+        axelrodSizer.Add(playMatchButton, pos=(4, 1))
+        axelrodSizer.Add(radioButton2, pos=(5, 0))
+        axelrodSizer.Add(viewDatabaseButton, pos=(5, 1))
+        axelrodSizer.Add(matchTextCtrl, pos=(6, 1))
+        axelrodSizer.Add(scoreTextCtrl, pos=(7, 1))
+        
+        axelrodSizer.AddGrowableRow(0)
+        axelrodSizer.AddGrowableRow(1)
+        axelrodSizer.AddGrowableRow(2)
+        axelrodSizer.AddGrowableRow(3)
+        axelrodSizer.AddGrowableRow(4)
+        axelrodSizer.AddGrowableRow(5)
+        axelrodSizer.AddGrowableRow(6)
+        axelrodSizer.AddGrowableCol(0)
+        axelrodSizer.AddGrowableCol(1)
+        
+        self.SetSizer(axelrodSizer)
         
 class MyApp(wx.App):
     def OnInit(self):
